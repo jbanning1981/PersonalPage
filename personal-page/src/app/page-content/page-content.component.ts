f73b62d276a409f329927b5c601a7c608ba7f400
Jim Banning <jbanning1981@users.noreply.github.com>
2021-12-21 01:50:51 -0600

    Add animations, start work on intro section about me.

--- personal-page/src/app/page-content/page-content.component.ts
+++ personal-page/src/app/page-content/page-content.component.ts
@@ -14,27 +14,15 @@ export class PageContentComponent implements OnInit {
   constructor(private jobService: JobService) { }
 
   ngOnInit(): void {
-    console.log('Page Content Loaded');
     this.JobHistory.push(new EmploymentRecord());
     this.LoadJobHistory();
   }
 
   LoadJobHistory(): void {
-    console.log('Load job history using API call');
     this.jobService.getJobHistory()
       .subscribe(data => {
         this.JobHistory = data;
-        console.log(data);
+        
       });
   }
-
-  DisplayJobHistory(): void {
-    console.log('Display job history. ngFor?');
-    console.log(this.JobHistory);
-    for (var index in this.JobHistory) {
-      console.log(this.JobHistory[index]);
-    }
-  }
-
-
 }
