69f31b85bcdea22abe523b683b1b3a7b59a8fac9
jbanning1981 <jbanning1981@users.noreply.github.com>
2024-06-23 01:09:16 -0500

    Switch from css to scss Continue navbar work

--- personalpage/src/components/navbar/navbar.tsx
+++ personalpage/src/components/navbar/navbar.tsx
@@ -1,24 +1,44 @@
-import { FC } from 'react';
-import { FontAwesomeIcon } from '@fortawesome/react-fontawesome';
-import { faGithub, faLinkedin } from '@fortawesome/free-brands-svg-icons';
+import { FontAwesomeIcon } from "@fortawesome/react-fontawesome";
+import { faGithub, faLinkedin } from "@fortawesome/free-brands-svg-icons";
+import "./navbar.scss";
+import portrait from "../../assets/Portrait.jpg";
 
-const Navbar: FC = () => {
-    return (
-        <nav>
-            <ul>
-                <li>
-                    <a href="https://github.com">
-                        <FontAwesomeIcon icon={faGithub} />
-                    </a>
-                </li>
-                <li>
-                    <a href="https://linkedin.com">
-                        <FontAwesomeIcon icon={faLinkedin} />
-                    </a>
-                </li>
+const Navbar = () => {
+  return (
+    <>
+      <nav className="navbar navbar-expand-lg">
+        <div className="container-fluid text-white">
+          <div className="navbar-brand">
+            <img
+              src={portrait}
+              alt="Jim Banning Avatar"
+              className="rounded-circle"
+            />
+          </div>
+          <div className="fs-2 align-items-center">
+            <span>Jim Banning - North Texas C#/.NET Dev</span>
+          </div>
+          <div className="ms-auto">
+            <ul className="navbar-nav">
+              <li className="nav-item">
+                <a className="nav-link text-white" href="https://github.com">
+                  <FontAwesomeIcon className="fa-2xl" icon={faGithub} />
+                </a>
+              </li>
+              <li className="nav-item">
+                <a className="nav-link text-white" href="https://linkedin.com">
+                  <FontAwesomeIcon className="fa-2xl" icon={faLinkedin} />
+                </a>
+              </li>
             </ul>
-        </nav>
-    );
+          </div>
+        </div>
+      </nav>
+      <div className="container-fluid navbar-footer">
+        <span className="p-1"></span>
+      </div>
+    </>
+  );
 };
 
 export default Navbar;
