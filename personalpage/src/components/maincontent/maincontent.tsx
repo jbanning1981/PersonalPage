cd809a1a287732c407fe27d40dfedc9b12498046
jbanning1981 <jbanning1981@users.noreply.github.com>
2024-08-31 19:45:14 -0500

    Adding API calls to load school and job data

--- personalpage/src/components/maincontent/maincontent.tsx
+++ personalpage/src/components/maincontent/maincontent.tsx
@@ -1,39 +1,106 @@
 import { useEffect, useState } from "react";
 import "./maincontent.scss";
 const MainContent = () => {
-  console.log('Content page loading...');
+  console.log("MainContent page started...");
+
   const [isLoading, setLoadingStatus] = useState(false);
   const [loadingText, setLoadingText] = useState("Loading...");
 
+  let schoolData = null;
   let isSchoolDataLoaded = false;
+  let isSchoolDataLoading = false;
   const getSchoolData = async () => {
-    
-    if(isSchoolDataLoaded) {
-      console.log('school data already loaded.');
+    if (isSchoolDataLoaded) {
+      console.log("School data already loaded. Exiting.");
       return;
     }
-    
-    //get data 
-      console.log('Starting API call.');
-      const schoolApiUrl = 'https://jbanning1981.blob.core.windows.net/assets/schoolHistory.json';
-      let schoolData = await fetch(schoolApiUrl, {
-        mode: 'no-cors'
-      });
-      //convert to json
-      isSchoolDataLoaded = true;
-  }
 
+    if (isSchoolDataLoading) {
+      console.log("School data load in progress. Exiting.");
+      return;
+    }
+
+    //get data
+    isSchoolDataLoading = true;
+    console.log("Starting API call.");
+    try {
+      const schoolApiUrl = "/schoolHistory.json";
+      const schoolResponse = await fetch(schoolApiUrl, {
+        method: "GET",
+        mode: "cors",
+        headers: {
+          "Content-Type": "application/json",
+        },
+      });
+
+      if (!schoolResponse.ok) {
+        throw new Error("Failed to load school data.")
+      }
+        
+        //convert to json
+        schoolData = await schoolResponse.json();
+        isSchoolDataLoaded = true;
+        console.log("School data load attempt successful.");
+    } catch (error) {
+      console.error(error);
+      isSchoolDataLoaded = false;
+      console.log("School load attempt failed.");
+    }
+    isSchoolDataLoading = false;
+  };
+
+  let jobData = null;
+  let isJobDataLoaded = false;
+  let isJobDataLoading = false;
+  const getJobData = async () => {
+    if (isJobDataLoaded) {
+      console.log("Job data already loaded. Exiting.");
+      return;
+    }
+
+    if (isJobDataLoading) {
+      console.log("Job data loading in progress. Exiting.");
+      return;
+    }
+
+    //get data
+    isJobDataLoading = true;
+    console.log("Starting Job API call.");
+    try {
+      const jobApiUrl = "/jobHistory.json";
+      const jobDataResponse = await fetch(jobApiUrl, {
+        method: "GET",
+        mode: "cors",
+        headers: {
+          "Content-Type": "application/json",
+        },
+      });
+
+      if (jobDataResponse.ok) {
+        //convert to json
+        jobData = await jobDataResponse.json();
+        isJobDataLoaded = true;
+        console.log("Job data load successful.");
+        console.log(jobData);
+      }
+    } catch (error) {
+      console.error(error);
+      isJobDataLoaded = false;
+      console.log("Load attempt failed.");
+    }
+    isJobDataLoading = false;
+  };
 
   useEffect(() => {
-    setLoadingStatus(true);
-
-    
-    setTimeout(function () {
+    if (isLoading != true) {
+      setLoadingStatus(true);
       getSchoolData();
+      getJobData();
       setLoadingText("API loaded Page content goes here");
       setLoadingStatus(false);
-    }, 5000);
+    }
 
+    setTimeout(function () {}, 5000000000);
   }, []);
 
   return (
@@ -42,15 +109,24 @@ const MainContent = () => {
         <div className="d-flex justify-content-center w-100">
           <div className="d-flex align-items-middle">
             <div className="align-self-center pt-neg10">
-              <div className="border rounded-circle bottom-spin spinner">
-                <div className="p-1">
-                  <div className="border rounded-circle top-spin">
-                    <div className="p-3">
-                      <div className="border rounded-circle core">
-                        <div className="p-3"></div>
+              <div className="d-flex align-self-center">
+                <div className="border rounded-circle outer-spin">
+                  <div className="border rounded-circle middle-spin spinner indigo-border-pulse">
+                    <div className="p-1">
+                      <div className="border rounded-circle inner-spin">
+                        <div className="p-1">
+                          <div className="border rounded-circle core">
+                            <div className="p-1"></div>
+                          </div>
+                        </div>
                       </div>
                     </div>
                   </div>
+                </div>
+              </div>
+              <div className="d-flex align-items-middle">
+                <div className="align-self-center justify-content-center pt-4 flex-grow-1">
+                  <p className="lh-1 fs-3 text-center">Loading...</p>
                 </div>
               </div>
             </div>
--- personalpage/src/components/sidebar/sidebar.tsx
+++ personalpage/src/components/sidebar/sidebar.tsx
@@ -9,8 +9,10 @@ const Sidebar = () => {
       id="sidebar"
       className="sidebar offcanvas-xl offcanvas-start d-flex w-25 border-end shadow"
     >
-        <div className="sidebar_content d-flex flex-wrap w-100 flex-column">
-          <div className="h-20">{/*Spacing div*/}</div>
+      <div className="sidebar_content d-flex flex-wrap w-100 flex-column">
+        <div className="h-20">{/*Spacing div*/}</div>
+        <div className="d-flex justify-content-center">
+
           <div className="sidebar_photo d-flex m-4 align-self-center">
             <div
               id="sidebarPortraitOuterWrapper"
@@ -24,25 +26,35 @@ const Sidebar = () => {
               </div>
             </div>
           </div>
-          <div className="d-flex flex-column text-center w-100">
-            <div className="fs-2 fst-italic py-1">Jim Banning</div>
-            <div className="fs-4 py-1">
-              North Texas C#/.NET Dev
-            </div>
+        </div>
+
+        <div className="d-flex flex-column text-center w-100">
+          <div className="fs-2 fst-italic py-1">Jim Banning</div>
+          <div className="fs-4 py-1">North Texas C#/.NET Dev</div>
+        </div>
+        <div className="d-flex w-100 justify-content-center">
+          <div className="p-2">
+            <a
+              className="text-dark"
+              href="https://github.com/jbanning1981/"
+              target="_blank"
+              rel="noreferrer noopener"
+            >
+              <FontAwesomeIcon className="fa-2x" icon={faGithub} />
+            </a>
           </div>
-          <div className="d-flex w-100 justify-content-center">
-            <div className="p-2">
-              <a className="text-dark" href="https://github.com/jbanning1981/" target="_blank" rel="noreferrer noopener">
-                <FontAwesomeIcon className="fa-2x" icon={faGithub} />
-              </a>
-            </div>
-            <div className="p-2">
-              <a className="text-dark" href="https://www.linkedin.com/in/jimbanning/" target="_blank" rel="noreferrer noopener">
-                <FontAwesomeIcon className="fa-2x" icon={faLinkedin} />
-              </a>
-            </div>
+          <div className="p-2">
+            <a
+              className="text-dark"
+              href="https://www.linkedin.com/in/jimbanning/"
+              target="_blank"
+              rel="noreferrer noopener"
+            >
+              <FontAwesomeIcon className="fa-2x" icon={faLinkedin} />
+            </a>
           </div>
-        </div>      
+        </div>
+      </div>
     </div>
   );
 };
